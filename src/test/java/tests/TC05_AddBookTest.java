b2f1ce50cbc29bd6c33d52108d5a91d8a639e218
Shikha singh <2023.shikhas@isu.ac.in>
2026-06-12 16:28:30 +0530

    Added Selenium Maven TestNG project

---
+++ src/test/java/tests/TC05_AddBookTest.java
@@ -0,0 +1,12 @@
+package tests;
+
+import org.testng.annotations.Test;
+
+public class TC05_AddBookTest {
+
+    @Test
+    public void addBook() {
+
+        System.out.println("Book Added Successfully");
+    }
+}
---
+++ src/test/java/tests/TC06_IssueBookTest.java
@@ -0,0 +1,12 @@
+package tests;
+
+import org.testng.annotations.Test;
+
+public class TC06_IssueBookTest {
+
+    @Test
+    public void issueBook() {
+
+        System.out.println("Book Issued Successfully");
+    }
+}
